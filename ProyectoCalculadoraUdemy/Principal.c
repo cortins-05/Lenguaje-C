7127be9942545e1ca84218751b6c7ed275516541
cortins-05 <lucasortins@gmail.com>
2025-03-07 14:30:09 +0100

    epa

---
+++ ProyectoCalculadoraUdemy/Principal.c
@@ -0,0 +1,13 @@
+#include "librerias.h"
+#include "declaracionesVariables.h"
+
+int main()
+{
+    #include "asignacionApuntadores.h"
+
+    printf("****Bienvenido a tu calculadora de Gastos****\n\n");
+    printf("Ingrese su sueldo mensual: ");
+    scanf("%f", &sueldo);
+
+    return 0;
+}
