e02792ef4c60d0f7558a51981dbc5338e728cc5c
cortins-05 <lucasortins@gmail.com>
2025-04-09 14:32:06 +0200

    hola

---
+++ Introduccion a las estructuras de datos/Estructuras Físicas y Lógicas.c
@@ -0,0 +1,16 @@
+/*
+Diferencia entre estructuras de datos Fisicas y Logicas:
+
+-> Fisicas:
+    Pueden ser los array o las listas ligadas, y guardan informacion
+
+-> Logicas:
+    Son las reglas por las cuales se definen como se introducen los valores en las estructuras físicas
+    Pueden ser:
+        Stack(Pila) -> Lineal -> El ultimo que entra, el primero que sale
+        Queues(Colas) -> Lineal -> El primero que entra el primero que sale
+        Trees(Arboles) -> No lineal -> Organizada por Jerarquia, conexion de nodos organizada
+        Graph(Grafos) -> No lineal -> Conexion de nodos 
+        Hash Table(Tablas Hash) -> Tabular
+
+*/
